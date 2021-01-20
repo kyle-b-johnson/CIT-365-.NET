3d07f5ef6ce5ef3cd9e9ed1fb4eb36aed3b1746b
kyle-b-johnson <johnson.kyleb@gmail.com>
2021-01-20 13:29:11 -0600

    Initial (complete) commit

---
+++ MyFirstConsoleApplication/MyFirstConsoleApplication/GlazerApp.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyFirstConsoleApplication
+{
+    class GlazerApp
+    {
+        public static void RunExample()
+        {
+            double width, height, woodLength, glassArea;
+            string widthString, heightString;
+            Console.WriteLine("Please enter a width for the window to be calculated: ");
+            widthString = Console.ReadLine();
+            width = double.Parse(widthString);
+            Console.WriteLine("Please enter a height for the window to be calculated: ");
+            heightString = Console.ReadLine();
+            height = double.Parse(heightString);
+            woodLength = 2 * (width + height) * 3.25;
+            glassArea = 2 * (width * height);
+            Console.WriteLine("The length of the wood is " +
+            woodLength + " feet");
+            Console.WriteLine("The area of the glass is " +
+            glassArea + " square metres");
+
+            return;
+        }
+    }
+}
---
+++ MyFirstConsoleApplication/MyFirstConsoleApplication/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+
+namespace MyFirstConsoleApplication
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetUserNameAndLocation();
+            ChristmasCountdown(DateTime.Now);
+            GlazerApp.RunExample();
+
+            Console.ReadKey();
+            return;
+        }
+        private static void GetUserNameAndLocation()
+        {
+            Person person = new Person();
+
+            Console.WriteLine("What is your name?");
+
+            person.name = Console.ReadLine();
+            
+            Console.WriteLine($"Hi {person.name}! Where are you from?");
+            person.location = Console.ReadLine();
+            
+            Console.WriteLine($"I have never been to {person.location} "+
+                "I bet it is nice.\nPress any key to continue...");
+
+            Console.ReadKey();
+
+            return;
+        }
+
+        private static void ChristmasCountdown(DateTime date)
+        {
+            Console.WriteLine($"Today's date is: {date.Date}");
+            int year = date.Year;
+            DateTime christmas = new DateTime(year, 12, 25, 00, 00, 00);
+            TimeSpan christmasDaysSpan = christmas.Subtract(date);
+            int christmasDays = (int) Math.Round(christmasDaysSpan.TotalDays);
+            Console.WriteLine($"There are {christmasDays} days until Christmas!" +
+                $"\nPress any key to continue..");
+
+            Console.ReadKey();
+
+            return;
+        }
+
+    
+    }
+
+   
+}
