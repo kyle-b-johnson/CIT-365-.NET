8a06de214d5d5c96bbd73f82f409d7c3808c809b
kyle-b-johnson <johnson.kyleb@gmail.com>
2021-03-13 22:18:18 -0600

    Initial Week10 Assignment commit

---
+++ SvalbardUniversity/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SvalbardUniversity.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SvalbardUniversity.Data;
+using SvalbardUniversity.Models.SchoolViewModels;
+using System.Data.Common;
+
+namespace SvalbardUniversity.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly SchoolContext _context;
+
+        public HomeController(SchoolContext context)
+        {
+            _context = context;
+        }
+
+       public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public async Task<ActionResult> About()
+        {
+            IQueryable<EnrollmentDateGroup> data =
+                from student in _context.Students
+                group student by student.EnrollmentDate into dateGroup
+                select new EnrollmentDateGroup()
+                {
+                    EnrollmentDate = dateGroup.Key,
+                    StudentCount = dateGroup.Count()
+                };
+            return View(await data.AsNoTracking().ToListAsync());
+        }
+    }
+}
---
+++ SvalbardUniversity/Migrations/20210309211120_NameColumns.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SvalbardUniversity.Migrations
+{
+    public partial class NameColumns : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "LastName",
+                table: "Student",
+                newName: "Last Name");
+
+            migrationBuilder.RenameColumn(
+                name: "FirstMidName",
+                table: "Student",
+                newName: "First Name");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Last Name",
+                table: "Student",
+                newName: "LastName");
+
+            migrationBuilder.RenameColumn(
+                name: "First Name",
+                table: "Student",
+                newName: "FirstMidName");
+        }
+    }
+}
